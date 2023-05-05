e4f6a5c86d70066ea9f8e0e0324052202ad1ca5b
Visu <kuppusamy.suganya@moneyforward.co.jp>
2023-05-05 14:32:58 +0530

    add runtime args

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello world!: \n arg 0 = " + args[0]);
          System.out.println("SONARQUBE_TOKEN: " + System.getenv("SONARQUBE_TOKEN"));
     }
 }
