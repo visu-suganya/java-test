041609188b4931bea1a2f5141d2259ca8774025b
Visu <kuppusamy.suganya@moneyforward.co.jp>
2023-05-05 10:35:16 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("SONARQUBE_TOKEN: " + System.getEnv("SONARQUBE_TOKEN"));
+        // System.out.println("SONARQUBE_TOKEN: " + System.getEnv("SONARQUBE_TOKEN"));
     }
 }
